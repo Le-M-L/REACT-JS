4d68a42aefb9188731c3138430a94ad0998ca44c
e <1093185442@qq.com>
2021-01-14 23:00:57 +0800

    首次提交

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+import './App.less';
+import { request } from './api/intercept.js';
+// console.log(request)
+let data = {
+  pageSize: 8,
+  pageNow: 1,
+  reportPersonId: '7a330a258ccc47858ae7f6f39d6030af',
+  reportPersonDeptId: 'dc2eb4a4397547f084d1ab6c25ffe687',
+  listType: 1,
+  eventState: '1,6,10,11,12',
+  deptLevel: 7,
+  timestamp: '1610622147000',
+  nonce: 'BD36sjBKHEcJpAtXsJ2HWJGRy3RzPxds',
+  dId: 'dc2eb4a4397547f084d1ab6c25ffe687',
+  roleIds: '9ef64a3545fe4afe96d6fd37020a10ed'
+}
+request('/UnifyEvent/list', 'get', data)
+.then(res => {
+  console.log(res)
+})
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      123
     </div>
   );
 }
