2c79ad294bc3cbedc4a337a0bb1d2cedd9bc72aa
Lml <1093185442@qq.com>
2021-05-20 10:31:40 +0800

    11

--- src/components/comps/Application/index.js
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-import AppProvider from './src/AppProvider.vue';
-import AppLogo from './src/AppLogo.vue';
-
-export { useAppProviderContext } from './src/useAppContext';
-export { AppLogo, AppProvider };
-
