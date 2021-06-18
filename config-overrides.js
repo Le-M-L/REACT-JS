b1413982c850b90cbb71b0ae1657141d585ecc14
Lml <1093185442@qq.com>
2021-06-18 18:05:30 +0800

    初始化

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,19 @@
-/* config-overrides.js */
+const { override, addDecoratorsLegacy, addLessLoader } = require('customize-cra');
 
-module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-    return config;
-  }
+module.exports = override(
+    // ES7 装饰器使用
+    addDecoratorsLegacy(),
+    addLessLoader({
+        lessOptions: {
+            javascriptEnabled: true,
+            // 可选地将url调整为相对的。当为false时，url已经相对于条目较少的文件。
+            // Optionally adjust URLs to be relative. When false, URLs are already relative to the entry less file.
+            relativeUrls: false,
+            modifyVars: { '@primary-color': '#A80000' },
+            // cssModules: {
+            //   // if you use CSS Modules, and custom `localIdentName`, default is '[local]--[hash:base64:5]'.
+            //   localIdentName: "[path][name]__[local]--[hash:base64:5]",
+            // }
+        },
+    })
+);
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,38 +1,12 @@
-import React, { PureComponent } from 'react'
-import { Button } from 'antd'
-import B from './B'
-import C from "./C"
-import MyContext from "./context"
-
-
-export default class Home extends PureComponent {
-
-    state = {
-        name: '12'
-    }
-
-    ok = () => {
-        this.setState({ name: '456' })
-        // this.props.history.push('/about')
-    }
-
-    render () {
-        console.log(this)
-
+import React, { Component } from 'react'
+import  "./index.model.less"
+import { Button } from "antd"
+export default class Home extends Component {
+    render() {
         return (
-            <div>
-                Home
-                <Button onClick={this.ok}> 点击修改 </Button>
-                <MyContext.Provider value={this.state} >
-                    <B />
-                </MyContext.Provider>
-                <MyContext.Consumer>
-                    {
-                        value => {
-                            return <C  {...this.state}/>
-                        }
-                    }
-                </MyContext.Consumer>
+            <div className="appBox">
+                    home
+                    <Button type="primary">按钮</Button>
             </div>
         )
     }
