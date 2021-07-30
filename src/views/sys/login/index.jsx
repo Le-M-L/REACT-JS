93eb09a12c463c4d3cec1caf1a53d719b43e2a1d
Lml <1093185442@qq.com>
2021-07-30 17:55:08 +0800

    样式修改

--- src/views/sys/login/index.jsx
+++ src/views/sys/login/index.jsx
@@ -1,10 +1,53 @@
 import React, { Component } from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+
 import "./index.less"
 export default class Login extends Component {
-    render() {
+    onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+    render () {
         return (
-            <div className="box" >
-                <div className="login">login</div>
+            <div className="loginBox" >
+                <div className="container">
+                    <div>
+                        <div></div>
+                        <div>
+                            <div className="container-form" >
+                                <Form
+                                    name="basic"
+                                    labelCol={{ span: 8 }}
+                                    wrapperCol={{ span: 16 }}
+                                    initialValues={{ remember: true }}
+                                    onFinish={this.onFinish}
+                                    onFinishFailed={this.onFinishFailed}
+                                >
+                                    <Form.Item label="用户名" name="username" rules={[{ required: true, message: '请输入用户名!', },]} >
+                                        <Input />
+                                    </Form.Item>
+
+                                    <Form.Item label="密码" name="password" rules={[{ required: true, message: '请输入密码！', },]}>
+                                        <Input.Password />
+                                    </Form.Item>
+                                    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16, }} >
+                                        <Checkbox>记住我</Checkbox>
+                                    </Form.Item>
+
+                                    <Form.Item wrapperCol={{ offset: 8, span: 16, }}>
+                                        <Button type="primary" htmlType="submit">
+                                            提交
+                                        </Button>
+                                    </Form.Item>
+                                </Form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
