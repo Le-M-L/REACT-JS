dc07d9c472bfb01eaccea3fbd0bd13eabb3cd566
Lml <1093185442@qq.com>
2021-08-20 14:12:34 +0800

    动态菜单

--- src/components/Menu/src/BasicMenu.jsx
+++ src/components/Menu/src/BasicMenu.jsx
@@ -2,22 +2,21 @@ import React, { Component } from 'react'
 import { Menu } from "antd"
 import PropTypes from "prop-types"
 import { withRouter } from "react-router-dom"
-import BasicSubMenuItem from "./components/BasicSubMenuItem"
 import "./index.less"
+const { SubMenu } = Menu;
+
 @withRouter
 class BasicMenu extends Component {
-    constructor() {
-        super()
-        this.state = {
-            selectedKeys: [], // 当前选中的菜单项 key
-            defaultSelectedKeys: [], // 初始选中的菜单 key 
-            mode:'inline',  
-            openKeys: [],  // 当前展开的 SubMenu 菜单项 key
-            inlineIndent: 24, // inline 模式的菜单缩进宽度
-            className: '',
-            subMenuOpenDelay: 0.2, // 用户鼠标进入子菜单后开启延时，单位：秒
-        }
+    state = {
+        // selectedKeys: [], // 当前选中的菜单项 key
+        // defaultSelectedKeys: [], // 初始选中的菜单 key 
+        mode: 'inline',
+        // openKeys: [],  // 当前展开的 SubMenu 菜单项 key
+        // inlineIndent: 24, // inline 模式的菜单缩进宽度
+        // className: '',
+        // subMenuOpenDelay: 0.2, // 用户鼠标进入子菜单后开启延时，单位：秒
     }
+    items = this.props.items || []
     static propTypes = {
         selectedKeys: PropTypes.array,
         defaultSelectedKeys: PropTypes.array,
@@ -28,32 +27,44 @@ class BasicMenu extends Component {
     }
 
     componentDidMount () {
-        console.log(this);
+        console.log(this.props);
+        this.setState({
+            openKeys: ["/admin/user", "/admin"],
+            selectedKeys: ["/admin/user"]
+        })
     }
 
-    handleOpenChange(openKeys) {
-        console.log(openKeys);
-
+    handleMenuClick = ({ key }) => {
+        if(this.props.location.pathname == key) return;
+        this.props.history.push(key)
+        this.setState({ selectedKeys: [key] })
     }
 
-    handleMenuClick ({ item, key, keyPath, domEvent }) {
-        console.log();
+    menuItem (item) {
+        return <Menu.Item key={item.path} >{item.title}</Menu.Item>
     }
 
-
-    render () {
-        const { items = [] } = this.props;
-        console.log(items);
-        return (
-            <Menu {...this.state} onClick={this.handleMenuClick} onOpenChange={this.handleOpenChange} >
+    subMenuItem (items) {
+        if (items.children) {
+            return <SubMenu key={items.path} title={items.title} >
                 {
-                    items.map(item => {
-                        return <BasicSubMenuItem item={item} key={item.path} />
-                    })
-                   
+                    items.children.map(item => this.subMenuItem(item))
+                }
+            </SubMenu>
+        }
+        return this.menuItem(items)
+    }
+    
+    render () {
+        return (
+            <Menu ref={e => this.menuRef = e} {...this.state}
+                onClick={this.handleMenuClick}
+                onOpenChange={(openKeys) => this.setState({ openKeys })} >
+                {
+                    this.items.map((item) => this.subMenuItem(item))
                 }
             </Menu>
         )
     }
 }
-export default  BasicMenu
+export default BasicMenu
